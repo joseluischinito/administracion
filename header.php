cfeb467c7ef97b39da074c8541c26159ac676b94
Henoc Dz <hdz@rfdz.mx>
2013-05-14 11:17:05 -0500

    Diseño base

---
+++ header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Administracion</title>
+
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+
+</head>
+<body>
+<header>
+
+	<div id="logo-h">
+		<a href=""><img src="imgs/escom.png"/></a>
+	</div>
+
+<nav>
+	<ul>
+		<li><a href="">Inicio</a></li>
+		<li><a href="">Estado de Resultados</a>
+			<ul>
+				<li><a href="">Consultar</a></li>
+				<li><a href="">Nuevo</a></li>
+			</ul>
+		</li>
+		<li><a href="">Balance General</a>
+			<ul>
+				<li><a href="">Consultar</a></li>
+				<li><a href="">Nuevo</a></li>
+			</ul>
+		</li>
+
+		<li><a href="">Otros</a>
+			<ul>
+				<li><a href="">Razones Financieras</a></li>
+				<li><a href="">Estado de Origen y Aplicacion</a></li>
+			</ul>
+		</li>
+
+		<li style="float:right;"><a href="./logout.php">Salir</a>
+	</ul>
+</nav>
+</header>
---
+++ login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Administracion</title>
+
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+
+</head>
+<body>
+
+<div id="login">
+<header>
+	<div id="logo-h">
+		<a href=""><img src="imgs/escom.png"/></a>
+	</div>
+</header>
+
+<section id="form">
+	<form>
+		<input type="text" name="usr" placeholder="Usuario"/>
+		<input type="password" name="usr" placeholder="Contrase&ntilde;a"/>
+
+		<input type="submit" value="Entrar"/>
+	</form>
+</section>
+
+</div>
+
+<?php
+	require_once('footer.php');
+?>
